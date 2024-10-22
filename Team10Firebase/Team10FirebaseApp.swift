e8837d25ed35d3a7f5428e9cf0d8200901e42ad3
Vicky Chen <vickyc@andrew.cmu.edu>
2024-10-21 20:42:48 -0400

    Initial Commit

---
+++ Team10Firebase/Team10FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team10FirebaseApp.swift
+//  Team10Firebase
+//
+//  Created by Vicky Chen on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Team10FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
